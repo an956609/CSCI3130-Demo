da14949000d9fb92bbcbd959b95a02ba61bf9120
Andre Gagne <an956609@dal.ca>
2018-01-20 12:38:00 -0400

    Implemented potential method for setting text but not tested

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void changeText(View v) {
+        TextView dst = (TextView) findViewById(R.id.textView2);
+        EditText src = (EditText) findViewById(R.id.editText);
+        dst.setText(src.getText().toString());
+    }
 }
